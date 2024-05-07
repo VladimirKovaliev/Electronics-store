26a57de242ad354873418fbe3975b7b9229ec725
VladimirKovaliev <kovaliev.v@list.ru>
2024-05-07 17:56:37 +0300

    add users model

---
+++ users/models.py
@@ -0,0 +1,28 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+# Create your models here.
+
+
+NULLABLE = {'null': True, 'blank': True}
+
+
+class User(AbstractUser):
+    username = None
+
+    email = models.EmailField(verbose_name='email', unique=True)
+    first_name = models.CharField(max_length=45, verbose_name='имя', **NULLABLE)
+    last_name = models.CharField(max_length=45, verbose_name='имя', **NULLABLE)
+    phone = models.CharField(max_length=15, verbose_name='телефон', **NULLABLE)
+    is_staff = models.BooleanField(default=False, verbose_name='сотрудник')
+    is_active = models.BooleanField(default=False, verbose_name='активность')
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return f'Пользователь - {self.email}'
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
